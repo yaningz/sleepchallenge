cffeea96c21a05d407145c87f405946210df50b9
lahuang4 <lahuang4@gmail.com>
2015-02-19 23:27:53 -0500

    Changed settings middleware to get server running

--- nextsleep/settings.py
+++ nextsleep/settings.py
@@ -49,8 +49,8 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.admin',
-    'django.contrib.admindocs',
+#    'django.contrib.admin',
+#    'django.contrib.admindocs',
 )
 
 ROOT_URLCONF = 'nextsleep.urls'
