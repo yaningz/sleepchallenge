23416c7d78fd05f746d0b83cb4d03f4c40bf1dc2
lahuang4 <lahuang4@gmail.com>
2015-03-01 15:43:13 -0500

    Basic models, will probably change

--- sleep/models.py
+++ sleep/models.py
@@ -1,6 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 class Wing(models.Model):
   name = models.CharField(max_length=200)
-  
+
+class UserProfile(models.Model):
+  user = models.OneToOneField(User)
+
+  wing = models.ForeignKey(Wing)
+  zs = model.TextField() # sleep data
+
+  def __unicode__(self):
+    return self.user.username
