44c135f606006e5f38b65b0caad6b714a4a06334
lahuang4 <lahuang4@gmail.com>
2015-03-28 23:37:59 -0400

    Enabled logging. To log something:
    1. At the beginning of the file, import logging
    2. Also at the beginning of the file, logger = logging.getLogger(__name__)
    3. Send a debug message with logger.debug("my debug message")
    4. Or, send an error message with logger.error("my error message")

--- nextsleep/settings.py
+++ nextsleep/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE_CLASSES = (
 #    'django.contrib.admindocs',
 )
 
-AUTHENTICAION_BACKENDS = (
+AUTHENTICATION_BACKENDS = (
     'mit.ScriptsRemoteUserBackend',
 )
 
@@ -63,6 +63,38 @@ ROOT_URLCONF = 'nextsleep.urls'
 
 WSGI_APPLICATION = 'nextsleep.wsgi.application'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'mysite.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'sleep': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
@@ -92,4 +124,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-
