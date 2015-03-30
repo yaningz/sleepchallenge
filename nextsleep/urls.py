0456f757772bf38817b78542d728799d59ab5e1c
lahuang4 <lahuang4@gmail.com>
2015-03-30 15:34:04 -0400

    OK sorry there was one more thing this is it I promise all the static stuff should be commented out by now

--- nextsleep/urls.py
+++ nextsleep/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
-from django.conf.urls.static import static
+# from django.conf.urls.static import static
 
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from nextsleep import settings
 
 admin.autodiscover()
